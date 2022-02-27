7018bab993774defc373e51c94637379a566d8f6
alnwlsn <alnwlsn@gmail.com>
2022-02-27 10:06:05 -0500

    g0 skip in the middle

--- laserg/rasterizer.py
+++ laserg/rasterizer.py
@@ -4,7 +4,7 @@
 
 linesPerMM = 2
 imageHeightMM = 30 #scale vertical size of image
-headStart = 5 #extra mm to go back at beginning of line to get a running start (overscan)
+headStart = 2 #extra mm to go back at beginning of line to get a running start (overscan)
 tailStop = 2 #extra mm to go after
 
 #center of image - offet assuming the origin of the workspace is in the lower left corner; offset is up and to the right
@@ -32,13 +32,16 @@ pixels = im2.load()
 for j in range(im2.size[1]):
     # f.write("S0\r\n")
     f.write("G0 X{:.4f} Y{:.4f} S0\r\n".format(round(centerX-(wMM/2)-headStart,4),round(((im2.size[1]-1-j)/linesPerMM)-(hMM/2)+centerY,4)))
-    lastPixel = 0
-    f.write("G1 X{:.4f}\r\n".format(round(centerX-(wMM/2),4)))
+    # f.write("G1 X{:.4f}\r\n".format(round(centerX-(wMM/2),4))) not actually needed
+    whiteSkipCount = 0
+    lastX = centerX-(wMM/2)-headStart #keep track of this; if we skip white with G0, we need to know where we skip from
     for i in range(im2.size[0]):
         pixel = 255-pixels[i,j]
+        
+        #skipping - we don't need to write a gcode line again if the same laser power is being used
         skipFlag = False
         #peek next pixel to see if it is the same color:
-        if(i+1<im2.size[0]):
+        if(i+1<im2.size[0]): #make sure we aren't past the end
             pixelF = 255-pixels[i+1,j]
             if(pixelF==pixel):
                 skipFlag = True
@@ -46,7 +49,18 @@ for j in range(im2.size[1]):
             if(pixel == 0): #if last pixel is white
                 skipFlag = True
         if(skipFlag==False):
-            f.write("G1 X{:.4f} S{:d}\r\n".format(round(centerX-(wMM/2)+(i+1)/linesPerMM,4),pixel))
+            thisX = centerX-(wMM/2)+(i+1)/linesPerMM
+            if(whiteSkipCount/linesPerMM>=headStart+(tailStop*2)):
+                print(i, "skip possible", lastX, thisX)
+                f.write("G1 X{:.4f} S0\r\n".format(round(lastX+tailStop,4)))
+                f.write("G0 X{:.4f}\r\n".format(round(thisX-headStart,4)))
+            f.write("G1 X{:.4f} S{:d}\r\n".format(round(thisX,4),pixel))
+            lastX = thisX
+            whiteSkipCount = 0
+        else:
+            if(pixel==0):
+                whiteSkipCount += 1
+        
         # print(pixel)
     f.write("G1 X{:.4f} S0\r\n".format(round(centerX+(wMM/2)+tailStop,4)))
 
@@ -55,4 +69,3 @@ for j in range(im2.size[1]):
 f.close()
 
 import laserRender
-laserRender
