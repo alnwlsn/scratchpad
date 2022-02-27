b5d2a4a2ebaa62eef6a3d182b79bb9fb41c26ecf
alnwlsn <alnwlsn@gmail.com>
2022-02-27 10:49:39 -0500

    working g0 skip on beginning and blank lines

--- laserg/rasterizer.py
+++ laserg/rasterizer.py
@@ -28,43 +28,63 @@ im2 = im.resize((int(sw),int(sh)))
 im2 = ImageOps.grayscale(im2)
 wMM = im2.size[0]/linesPerMM
 hMM = im2.size[1]/linesPerMM
-pixels = im2.load()
-for j in range(im2.size[1]):
-    # f.write("S0\r\n")
-    f.write("G0 X{:.4f} Y{:.4f} S0\r\n".format(round(centerX-(wMM/2)-headStart,4),round(((im2.size[1]-1-j)/linesPerMM)-(hMM/2)+centerY,4)))
-    # f.write("G1 X{:.4f}\r\n".format(round(centerX-(wMM/2),4))) not actually needed
-    whiteSkipCount = 0
-    lastX = centerX-(wMM/2)-headStart #keep track of this; if we skip white with G0, we need to know where we skip from
-    for i in range(im2.size[0]):
-        pixel = 255-pixels[i,j]
-        
-        #skipping - we don't need to write a gcode line again if the same laser power is being used
-        skipFlag = False
-        #peek next pixel to see if it is the same color:
-        if(i+1<im2.size[0]): #make sure we aren't past the end
-            pixelF = 255-pixels[i+1,j]
-            if(pixelF==pixel):
-                skipFlag = True
-        else:
-            if(pixel == 0): #if last pixel is white
-                skipFlag = True
-        if(skipFlag==False):
-            thisX = centerX-(wMM/2)+(i+1)/linesPerMM
-            if(whiteSkipCount/linesPerMM>=headStart+(tailStop*2)):
-                print(i, "skip possible", lastX, thisX)
-                f.write("G1 X{:.4f} S0\r\n".format(round(lastX+tailStop,4)))
-                f.write("G0 X{:.4f}\r\n".format(round(thisX-headStart,4)))
-            f.write("G1 X{:.4f} S{:d}\r\n".format(round(thisX,4),pixel))
-            lastX = thisX
-            whiteSkipCount = 0
-        else:
-            if(pixel==0):
-                whiteSkipCount += 1
-        
-        # print(pixel)
-    f.write("G1 X{:.4f} S0\r\n".format(round(centerX+(wMM/2)+tailStop,4)))
 
-    # break
+#black and white image
+thresh = 128
+fn = lambda x : 255 if x > thresh else 0
+im2 = im2.convert('L').point(fn, mode='1')
+
+pixels = im2.load()
+
+
+for j in range(im2.size[1]):
+    skipWholeLine = False
+    #search for the first burning pixel
+    fp = 0
+    while(fp<im2.size[0]):
+        if(255-pixels[fp,j]>0):
+            break
+        fp += 1
+    if(fp==im2.size[0]): #no need to do anything on the line if it is white
+        skipWholeLine = True
+
+    if(not skipWholeLine):        
+        # f.write("S0\r\n")
+        lastX = centerX-(wMM/2)+(fp+1)/linesPerMM #keep track of this; if we skip white with G0, we need to know where we skip from
+        f.write("G0 X{:.4f} Y{:.4f} S0\r\n".format(round(lastX-headStart,4),round(((im2.size[1]-1-j)/linesPerMM)-(hMM/2)+centerY,4)))
+        f.write("G1 X{:.4f} S0\r\n".format(round(lastX,4))) 
+        whiteSkipCount = 0
+        
+        for i in range(fp-1,im2.size[0]):
+            pixel = 255-pixels[i,j]
+            
+            #skipping - we don't need to write a gcode line again if the same laser power is being used
+            skipFlag = False
+            #peek next pixel to see if it is the same color:
+            if(i+1<im2.size[0]): #make sure we aren't past the end
+                pixelF = 255-pixels[i+1,j]
+                if(pixelF==pixel):
+                    skipFlag = True
+            else:
+                if(pixel == 0): #if last pixel is white
+                    skipFlag = True
+            if(skipFlag==False):
+                thisX = centerX-(wMM/2)+(i+1)/linesPerMM
+                if(whiteSkipCount/linesPerMM>=headStart+(tailStop*2)):
+                    f.write("G1 X{:.4f} S0\r\n".format(round(lastX+tailStop,4)))
+                    f.write("G0 X{:.4f}\r\n".format(round(thisX-headStart,4)))
+                f.write("G1 X{:.4f} S{:d}\r\n".format(round(thisX,4),pixel))
+                lastX = thisX
+                whiteSkipCount = 0
+            else:
+                if(pixel==0):
+                    whiteSkipCount += 1
+            
+            # print(pixel)
+        f.write("G1 X{:.4f} S0\r\n".format(round(centerX+(wMM/2)+tailStop,4)))
+
+    # if(j==5):
+    #     break
 
 f.close()
 
